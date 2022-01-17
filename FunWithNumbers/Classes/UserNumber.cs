d1329e773be88405d2df8bd55227264289c27283
Andrew <drew.m2340@gmail.com>
2022-01-16 20:33:26 -0500

    Created UserNumber class

---
+++ FunWithNumbers/Classes/UserNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunWithNumbers.Classes
+{
+    class UserNumber
+    {
+        //Member Variables
+        private double _number;
+        private long _longNumber;
+
+        //Constructor
+        public UserNumber(double number)
+        {
+            SetNumber(number);
+        }
+
+        //Getters & Setters
+        public double GetNumber()
+        {
+            return _number;
+        }
+
+        public void SetNumber(double number)
+        {
+            _number = number;
+        }
+
+        public long GetLongNumber()
+        {
+            return _longNumber;
+        }
+
+        //Special setter for _longNumber to convert the user's double input into a long
+        public void SetLongNumber()
+        {
+            _longNumber = (long)_number;
+        }
+    }
+}
--- FunWithNumbers/Program.cs
+++ FunWithNumbers/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ * FunWithNumbers
+ * 
+ * Written by Andrew McKay
+ * Last Edited 1/16/21
+ * 
+ * The purpose of this program is to demonstrate knowledge in console input and output in C#.
+ * The program will prompt the user to enter 5 numbers, calculate the sum and average of those
+ * numbers, and display both calculations' results to the user.
+ * 
+ */
+
+using System;
 
 namespace FunWithNumbers
 {
@@ -6,7 +18,7 @@ namespace FunWithNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
